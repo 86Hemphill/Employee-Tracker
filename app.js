04d4d9266dc35ee884366ad482c29f5e600b4c22
Logan Hemphill <logan.hemphill86@gmail.com>
2020-05-18 19:05:30 -0400

    added switch/case

--- app.js
+++ app.js
@@ -12,11 +12,64 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log(connection.threadId);
-  connection.query("SELECT * FROM employee;", function(err, res) {
-    if (err) throw err;
-    console.table(res);
-    })
-//   start();
+//   connection.query("SELECT * FROM employee;", function(err, res) {
+//     if (err) throw err;
+//     console.table(res);
+//     })
+  start();
 });
 
-// function
+async function start() {
+    try {
+      const questions = await inquirer.prompt([
+        {
+          type: "rawlist",
+          message: "What would you like to do?",
+          name: "startWhat",
+          choices: [
+              "View All Departments",
+              "View All Roles",
+              "View All Employees",
+              "Add A Department",
+              "Add A Role",
+              "Add An Employee",
+              "Exit"
+          ]
+        }])
+        .then(function(answer) {
+            switch (answer.startWhat) {
+            case "View All Departments":
+              break;
+      
+            case "View All Roles":
+              break;
+      
+            case "View All Employees":
+              break;
+      
+            case "Add A Department":
+              break;
+
+            case "Add A Role":
+              break;
+
+            case "Add An Employee":
+              break;
+      
+            case "Exit":
+              connection.end();
+              break;
+            }
+          });
+      console.log(questions);
+    //   const userInfo = await api.getUser(questions.username);
+    //   questions.email = userInfo.email;
+    //   questions.profilePic = userInfo.avatar_url;
+    //   const answers = generateMarkdown(questions);
+    //   fs.writeFile(filename, answers, function () {
+    //     console.log("Successfully generated README.md file!");
+    //   });
+    } catch (err) {
+      console.log(err);
+    }
+  }
