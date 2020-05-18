b97997ae299ab26858f80c5b6f607f66c6648c3b
Logan Hemphill <logan.hemphill86@gmail.com>
2020-05-18 18:00:57 -0400

    schema complete

--- employee_DB-schema.sql
+++ employee_DB-schema.sql
@@ -27,6 +27,23 @@ CREATE TABLE department (
   PRIMARY KEY (id)
 );
 
+INSERT INTO department (name)
+VALUES ("Engineering"), ("Finance"), ("Legal"), ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 4), ("Salesperson", 80000, 4), 
+("Lead Engineer", 150000, 1), ("Software Engineer", 120000, 1), 
+("Accountant", 125000, 2), ("Legal Team Lead", 250000, 3), ("Lawyer", 190000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 3),
+("Mike", "Chan", 2, 1),
+("Ashley", "Rodriguez", 3, NULL),
+("Kevin", "Tupik", 4, 3),
+("Malia", "Brown", 5, NULL),
+("Sarah", "Lourd", 6, NULL),
+("Tom", "Allen", 7, 6);
+
 SELECT * FROM employee;
 SELECT * FROM role;
 SELECT * FROM department;
